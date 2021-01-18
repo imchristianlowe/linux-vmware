38ec433b07ffe75f750d2a11e97ac2a306980c84
Christian Lowe <clowe@localhost.localdomain>
2021-01-18 08:46:37 -0500

    Initial commit

---
+++ sign-modules.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+openssl req -new -x509 -newkey rsa:2048 -keyout MOK.priv -outform DER -out MOK.der -nodes -days 36500 -subj "/CN=VMware/"
+
+sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmmon) 
+
+sudo /usr/src/linux-headers-`uname -r`/scripts/sign-file sha256 ./MOK.priv ./MOK.der $(modinfo -n vmnet) 
